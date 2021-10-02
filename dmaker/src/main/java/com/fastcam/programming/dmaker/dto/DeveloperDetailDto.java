ff4c9d9a9ab040ce62a20ee4474bc6d4cedb4bb0
Skye <80323673+hayoonpark@users.noreply.github.com>
2021-10-03 07:56:01 +0900

    Add files via upload

---
+++ dmaker/src/main/java/com/fastcam/programming/dmaker/dto/DeveloperDetailDto.java
@@ -0,0 +1,36 @@
+package com.fastcam.programming.dmaker.dto;
+
+import com.fastcam.programming.dmaker.entity.Developer;
+import com.fastcam.programming.dmaker.type.DeveloperLevel;
+import com.fastcam.programming.dmaker.type.DeveloperSkillType;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeveloperDetailDto {
+    private DeveloperLevel developerLevel;
+    private DeveloperSkillType developerSkillType;
+    private Integer experienceYears;
+    private String memberId;
+    private String name;
+    private Integer age;
+
+    public  static  DeveloperDetailDto fromEntity(Developer developer){
+        return DeveloperDetailDto.builder()
+                .developerLevel(developer.getDeveloperLevel())
+                .developerSkillType(developer.getDeveloperSkillType())
+                .experienceYears(developer.getExperienceYears())
+                .memberId(developer.getMemberId())
+                .name(developer.getName())
+                .age(developer.getAge())
+                .build();
+    }
+}
